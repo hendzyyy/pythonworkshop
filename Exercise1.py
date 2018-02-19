44b18f23f09096aa5db3e37cbf2daad4281237a8
Jann Hendrick Ugalino <36612278+hendzyyy@users.noreply.github.com>
2018-02-19 12:43:02 +0800

    Add files via upload

---
+++ Exercise1.py
@@ -0,0 +1,6 @@
+money= input("How much money do you have?")
+print "You have", money
+iPhone=50000
+print "You can afford",money/iPhone,"unit/s of iPhone/s."
+print "You need", iPhone-(money%iPhone), "to buy an additional unit."
+
